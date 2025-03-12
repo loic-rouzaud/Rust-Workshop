6ee8211f80c094c3026051df9a2fddd492158352
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-03-12 15:55:32 +0100

    [ADD] - remercier chatgpt pour l'haskell quand meme

--- benchmarking/count.hs
+++ benchmarking/count.hs
@@ -5,10 +5,12 @@ approxPi n = 4 * sum [((-1) ** fromIntegral k) / (2 * fromIntegral k + 1) | k <-
 
 main :: IO ()
 main = do
-    let iterations = 1000000  -- Nombre d'itérations
+    let iterations = 1000000
     start <- getCPUTime
     let piApprox = approxPi iterations
     end <- getCPUTime
     let timeMs = fromIntegral (end - start) / (10^9)
 
     putStrLn $ "Temps d'exécution en Haskell : " ++ show timeMs ++ " ms"
+
+-- merci chatgpt pour l'haskell tu gères
